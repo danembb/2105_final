9c1f0b14c989ba3f766abfd0083f0a4e39633b3c
danembb <dbrophy720@gmail.com>
2021-06-16 09:12:31 -0600

    Test attributes for ::train class

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -11,5 +11,12 @@ RSpec.describe Train do
 
       expect(@train1).to be_a(Train)
     end
+
+    it "has attributes" do
+
+      expect(@train1.name).to eq("Thomas")
+      expect(@train1.type).to eq("Tank")
+      expect(@train1.cargo).to eq({})
+    end
   end
 end
