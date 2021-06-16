98195cb642a82de0b5431849d3de20426b769b3a
danembb <dbrophy720@gmail.com>
2021-06-16 09:07:58 -0600

    Test attributes for ::car class

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,11 +1,21 @@
 require "./lib/car"
 
 RSpec.describe Car do
+  before :each do
+    @car1 = Car.new({type: "Mail", weight: 5})
+  end
+
   describe "instantiation" do
     it "::new" do
-      car1 = Car.new({type: "Mail", weight: 5})
 
-      expect(car1).to be_a(Car)
+
+      expect(@car1).to be_a(Car)
+    end
+
+    it "has attributes" do
+
+      expect(@car1.type).to eq("Mail")
+      expect(@car1.weight).to eq(5)
     end
   end
 end
