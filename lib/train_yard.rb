d4e16d8e6f46ae78c3c405336077856e9e97634f
danembb <dbrophy720@gmail.com>
2021-06-16 09:28:42 -0600

    Test attributes for ::trainyard class

--- lib/train_yard.rb
+++ lib/train_yard.rb
@@ -1,5 +1,8 @@
 class TrainYard
+  attr_reader :location
 
   def initialize(train_yard_info)
+    @location = train_yard_info[:location]
+
   end
 end
--- spec/train_yard_spec.rb
+++ spec/train_yard_spec.rb
@@ -6,11 +6,16 @@ RSpec.describe TrainYard do
   before :each do
     @train_yard = TrainYard.new({location: 'Brighton'})
   end
-  
+
   describe "initialize" do
     it "::new" do
 
       expect(@train_yard).to be_a(TrainYard)
+    end
+
+    it "has attributes" do
+
+      expect(@train_yard.location).to eq("Brighton")
     end
   end
 end
