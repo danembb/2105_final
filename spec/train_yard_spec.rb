a03a3f63bc8f34a649584844a7c3885fc91f11f3
danembb <dbrophy720@gmail.com>
2021-06-16 09:26:56 -0600

    Create ::trainyard class

---
+++ spec/train_yard_spec.rb
@@ -0,0 +1,16 @@
+require "./lib/car"
+require "./lib/train"
+require './lib/train_yard'
+
+RSpec.describe TrainYard do
+  before :each do
+    @train_yard = TrainYard.new({location: 'Brighton'})
+  end
+  
+  describe "initialize" do
+    it "::new" do
+
+      expect(@train_yard).to be_a(TrainYard)
+    end
+  end
+end
